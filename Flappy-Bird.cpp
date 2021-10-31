60f567ea196d9cd0bbffd222743c9769e7528431
Maryam <maryam.eloraby@gmail.com>
2021-10-31 14:22:24 +0200

    initial

--- Flappy-Bird.cpp
+++ Flappy-Bird.cpp
@@ -24,13 +24,14 @@ double xdash = 0.0;
 
 int speY = 0;
 
-int start = 0;
-int sset = rand() % 3 + 1; 
+int start = screenwidth - screenwidth / 3;
+
+int startgame = 0;
+int sset = rand() % 3 + 1;
 int gameon = 0;
 int endgame = 0;
 
-
-
+list <int> sets = {};
 
 
 // A good hackaround found here for to_string problems with GCC
@@ -75,7 +76,7 @@ void drawbird() {
 }
 
 void drawrectangle(int x, int y,int w, int h) {
-
+	
 	glBegin(GL_POLYGON);
 		glVertex3f(x, y, 0.0f);
 		glVertex3f(x + w, y, 0.0f);
@@ -84,98 +85,83 @@ void drawrectangle(int x, int y,int w, int h) {
 	glEnd();
 
 }
-
-void set1(int bar1set1,int bar2set1,int bar3set1) {
+void set1(int startpos) {
 
 	glColor3f(0, 1, 0.5);
 
-	drawrectangle(bar1set1, 0, barwidth, 140);
-	drawrectangle(bar1set1, 270, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 140);
+	drawrectangle(startpos, 270, barwidth, 195);
+	startpos += 150;
 
-	drawrectangle(bar2set1, 0, barwidth, 170);
-	drawrectangle(bar2set1, 300, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 170);
+	drawrectangle(startpos, 300, barwidth, 195);
+	startpos += 150;
 
-	drawrectangle(bar3set1, 0, barwidth, 200);
-	drawrectangle(bar3set1, 330, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 200);
+	drawrectangle(startpos, 330, barwidth, 195);
+	
+
 }
-void set2(int bar1set2,int bar2set2 , int bar3set2) {
+void set2(int startpos) {
+
+
 	glColor3f(0, 0.7, 0.5);
+	drawrectangle(startpos, 0, barwidth, 230);
+	drawrectangle(startpos, 360, barwidth, 195);
+	startpos += 150;
 
-	drawrectangle(bar1set2, 0, barwidth, 230);
-	drawrectangle(bar1set2, 360, barwidth, 195);
 
-	drawrectangle(bar2set2, 0, barwidth, 170);
-	drawrectangle(bar2set2, 300, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 170);
+	drawrectangle(startpos, 300, barwidth, 195);
+	startpos += 150;
 
-	drawrectangle(bar3set2, 0, barwidth, 230);
-	drawrectangle(bar3set2, 360, barwidth, 195);
+
+	drawrectangle(startpos, 0, barwidth, 230);
+	drawrectangle(startpos, 360, barwidth, 195);
+	
+	
 }
-void set3(int bar1set3, int bar2set3,int bar3set3) {
+void set3(int startpos) {
 
 	glColor3f(0, 0.55, 0.5);
 
-	drawrectangle(bar1set3, 0, barwidth, 230);
-	drawrectangle(bar1set3, 360, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 230);
+	drawrectangle(startpos, 360, barwidth, 195);
+	startpos += 150;
 
-	drawrectangle(bar2set3, 0, barwidth, 170);
-	drawrectangle(bar2set3, 300, barwidth, 195);
 
-	drawrectangle(bar3set3, 0, barwidth, 170);
-	drawrectangle(bar3set3, 300, barwidth, 195);
+	drawrectangle(startpos, 0, barwidth, 170);
+	drawrectangle(startpos, 300, barwidth, 195);
+	startpos += 150;
+
+
+	drawrectangle(startpos, 0, barwidth, 170);
+	drawrectangle(startpos, 300, barwidth, 195);
+	
 }
 
-void drawBars() {
+
+void drawBars(int sset) {
 
 	glPushMatrix();
 	glTranslated(-xdash, 0, 0);
 	if (gameon) {
 		xdash += 3;
 	}
+
 	
-
-	int startm = (screenwidth / 2);
-	int start1 = startm;
-	int start2 = start1 + 480 + 160;
-	int start3 = start2+ 480 + 160;
-
-	int bar1set1 = start1;
-	int bar2set1 = start1 + 160;
-	int bar3set1 = start1 + (160 * 2);
-
-	int bar1set2 = start2;
-	int bar2set2 = start2 + 160;
-	int bar3set2 = start2 + (160 * 2);
-
-	int bar1set3 = start3;
-	int bar2set3 = start3 + 160;
-	int bar3set3 = start3 + (160 * 2);
-
-	switch (sset) {
+	int set = sset;
+	switch (set) {
 	case 1:
-	
-		set1(bar1set1,bar2set1,bar3set1);
-		set2(bar1set2, bar2set2, bar3set2);
-		set3(bar1set3, bar2set3, bar3set3);
+		set1(start);
 		break;
 
 	case 2:
-
-		start2 = startm;
-		start3 = start2 + 480 + 160;
-		start1 = start3 + 480 + 160;
-		set2(bar1set2, bar2set2, bar3set2);
-	
+		set2(start);
 		break;
 	case 3:
-
-		start3 = startm;
-		start1 = start3 + 480 + 160;
-		start2 = start1 + 480 + 160;
-		set3(bar1set3, bar2set3, bar3set3);
-		
-
+		set3(start);
 		break;
-
 
 	default:
 		break;
@@ -198,7 +184,7 @@ void spe(int k, int x, int y)// keyboard special key function takes 3 parameters
 void keyUp(unsigned char k, int x, int y)//keyboard up function is called whenever the keyboard key is released
 {
 	if (k)
-		start = 1;
+		startgame = 1;
 	glutPostRedisplay();//redisplay to update the screen with the changed
 }
 
@@ -235,18 +221,26 @@ void drawTimer() {
 void Display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+
+	
 	glPushMatrix();
-	if (!start) {
+	if (!startgame) {
 		drawBitmapText("Press any key to start", screenwidth / 2 - 70, screenheight / 2, 0);
 	}
 	else {
 		gameon = 1;
+
 	}
 	glPopMatrix();
-	drawBars();
-
-	drawbird();
 	
+	drawbird();
+
+	drawBars(sset);
+
+
+
+
+
 
 	glFlush();
 }
@@ -263,6 +257,8 @@ void main(int argc, char** argr) {
     glutSpecialFunc(spe);			//call the keyboard special keys function
 	glutKeyboardUpFunc(keyUp);		//call the keyboard up function
 
+
+	
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	gluOrtho2D(0.0, screenwidth, 0.0, screenheight);
